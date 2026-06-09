3ece78d125c90ac070447214f70d942a2d435381
Edvard Filistovič <efilistovic@gmail.com>
2026-06-09 22:55:14 +0300

    [lightning-ln882h] Rename to lt_ble_mac_get (ln_ble_mac_get collides with SDK co_utils)

--- cores/lightning-ln882h/base/api/lt_ble.c
+++ cores/lightning-ln882h/base/api/lt_ble.c
@@ -12,7 +12,7 @@
 
 #include "lt_ble.h"
 
-bool ln_ble_mac_get(uint8_t out[6]) {
+bool lt_ble_mac_get(uint8_t out[6]) {
 	if (sysparam_sta_mac_get(out) != SYSPARAM_ERR_NONE)
 		return false;
 	uint32_t nic = ((((uint32_t)out[3] << 16) | ((uint32_t)out[4] << 8) | out[5]) + 1) & 0xFFFFFF;
