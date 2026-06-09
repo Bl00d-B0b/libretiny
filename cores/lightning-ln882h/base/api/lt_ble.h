307cf494cc62c588c513f8f286e8589148e84fb6
Edvard Filistovič <efilistovic@gmail.com>
2026-06-09 22:18:26 +0300

    [lightning-ln882h] Rename ln882h_ble_mac_get -> ln_ble_mac_get

--- cores/lightning-ln882h/base/api/lt_ble.h
+++ cores/lightning-ln882h/base/api/lt_ble.h
@@ -9,7 +9,7 @@ extern "C" {
 
 // Derive the BLE MAC from the WiFi STA MAC: low 24-bit NIC + 1 (wraparound),
 // OUI unchanged, written MSB-first (out[0] = OUI).
-void ln882h_ble_mac_get(uint8_t out[6]);
+void ln_ble_mac_get(uint8_t out[6]);
 
 #ifdef __cplusplus
 }
