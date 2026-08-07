cea77410aaa757f0b647ab5da27ea62ff35eb26b
Bl00d-B0b <efilistovic@gmail.com>
2026-08-07 17:14:16 +0300

    realtek-ambd: optional hardfault dumper (stacked frame + CFSR/HFSR/BFAR, halts instead of resetting)

--- cores/realtek-ambd/base/api/lt_init.c
+++ cores/realtek-ambd/base/api/lt_init.c
@@ -5,7 +5,14 @@
 
 extern uint8_t lt_uart_port;
 
+#if LT_AMBD_FAULT_DUMP
+extern void lt_fault_dump_install(void);
+#endif
+
 void lt_init_family() {
+#if LT_AMBD_FAULT_DUMP
+	lt_fault_dump_install();
+#endif
 	// route LibreTiny's printf port to the family default (LOGUART on AmebaD)
 	lt_uart_port = LT_UART_DEFAULT_PORT;
 	// silence the SDK's own DBG_8195A chatter (diag.h globals)
